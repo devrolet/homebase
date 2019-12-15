14e1eab8fa0310ded9e1701b1875119e7e712118
devrolet <chetdhill@gmail.com>
2019-12-15 16:12:16 -0500

    Finish Edit Post Functionality, Fix Errors

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -12,9 +12,9 @@ import { Post } from '../post.model';
 export class PostCreateComponent implements OnInit{
   enteredTitle = '';
   enteredContent = '';
+  post: Post;
   private mode = 'create';
   private postId: string;
-  private post: Post;
 
   constructor(public postsService: PostsService, public route: ActivatedRoute) {}
 
@@ -23,7 +23,9 @@ export class PostCreateComponent implements OnInit{
       if (paramMap.has('postId')) {
         this.mode = 'edit';
         this.postId = paramMap.get('postId');
-        this.post = this.postsService.getPost(this.postId);
+        this.postsService.getPost(this.postId).subscribe(postData => {
+          this.post = {id: postData._id, title: postData.title, content: postData.content};
+        });
       } else {
         this.mode = 'create';
         this.postId = null;
@@ -31,11 +33,20 @@ export class PostCreateComponent implements OnInit{
     });
   }
 
-  onAddPost(form: NgForm) {
+  onSavePost(form: NgForm) {
     if (form.invalid) {
       return;
     }
-    this.postsService.addPost(form.value.title, form.value.content);
+    if(this.mode === 'create') {
+      this.postsService.addPost(form.value.title, form.value.content);
+    } else {
+      this.postsService.updatePost(
+        this.postId,
+        form.value.title,
+        form.value.content
+      );
+    }
+    
     form.resetForm();
   }
 }
