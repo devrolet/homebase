d7bda8d2f8875bf9223dd5076cc67cc2ade2a38c
devrolet <chetdhill@gmail.com>
2020-01-03 10:00:33 -0500

    Last Update before Launch

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ mongoose.connect("mongodb+srv://chill-admin:" + process.env.MONGO_ATLAS_PW + "@c
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join('backend/images')));
+app.use("/images", express.static(path.join('images')));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://homebase-env.sjuj8awv52.us-west-2.elasticbeanstalk.com/api'
 };
